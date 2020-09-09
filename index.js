1036c55f2636727beb2c6927984cc06e103fd3a9
Alberto <betocantu93@gmail.com>
2020-09-09 18:51:43 -0500

    use css for now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const resolve = require("resolve");
 const BroccoliMergeTrees = require("broccoli-merge-trees");
 const writeFile = require("broccoli-file-creator");
 const Funnel = require("broccoli-funnel");
-const EuiScssFilter = require('./lib/elastic-eui-scss-filter');
+const EuiScssFilter = require("./lib/elastic-eui-scss-filter");
 
 module.exports = {
   name: require("./package").name,
@@ -30,59 +30,48 @@ module.exports = {
       } while (current.parent.parent && (current = current.parent));
     }
 
-    this.emberEuiOptions = Object.assign(
-      { ...this.config },
-      app.options["ember-eui"]
-    );
+    this.emberEuiOptions = Object.assign({}, app.options["ember-eui"]);
 
+   
+    if(this.emberEuiOptions.theme) {
+      app.import(`node_modules/@elastic/eui/dist/eui_theme_${this.emberEuiOptions.theme}.min.css`);
+    } else {
+      app.import("node_modules/@elastic/eui/dist/eui_theme_light.min.css");
+    }
+
+    
   },
 
-  config() {
-    return {
-      "ember-eui": {
-        theme: "light",
-      },
-    };
-  },
+  
 
-  treeForStyles(tree) {
-    let selectedTheme = this.emberEuiOptions.theme || this.config.theme;
-    console.log(selectedTheme);
+  // TODO: Currently the performance of recompiling sass on every change are serious, find a way to improve them.
+  // treeForStyles(tree) {
+  //   let selectedTheme = this.emberEuiOptions.theme || this.config.theme;
 
-    let euiScssFiles = new Funnel(this.pathBase("@elastic/eui"), {
-      srcDir: "/src",
-      include: ["**/*.scss"],
-      destDir: "elastic-eui",
-      annotation: "ElasticEUIScssFunnel",
-    });
+  //   let euiScssFiles = new Funnel(this.pathBase("@elastic/eui"), {
+  //     srcDir: "/src",
+  //     include: ["**/*.scss"],
+  //     destDir: "elastic-eui",
+  //     annotation: "ElasticEUIScssFunnel",
+  //   });
 
-    euiScssFiles = new EuiScssFilter(euiScssFiles);
+  //   euiScssFiles = new EuiScssFilter(euiScssFiles);
 
-    console.log(euiScssFiles);
-    let importer = writeFile(
-      "ember-eui-components.scss",
-      `@import './elastic-eui/theme_light.scss';`
-    );
+  //   console.log(euiScssFiles);
+  //   let importer = writeFile(
+  //     "ember-eui-components.scss",
+  //     `@import './elastic-eui/theme_light.scss';`
+  //   );
 
-    let mergedTrees = new BroccoliMergeTrees([euiScssFiles, importer, tree], {
-      overwrite: true,
-    });
-    return this._super.treeForStyles(mergedTrees);
-  },
+  //   let mergedTrees = new BroccoliMergeTrees([euiScssFiles, importer, tree], {
+  //     overwrite: true,
+  //   });
+  //   return this._super.treeForStyles(mergedTrees);
+  // },
 
-  /*
-    Rely on the `resolve` package to mimic node's resolve algorithm.
-    It finds the angular-material-source module in a manner that works for npm 2.x,
-    3.x, and yarn in both the addon itself and projects that depend on this addon.
-    This is an edge case b/c angular-material-source does not have a main
-    module we can require.resolve through node itself and similarily ember-cli
-    does not have such a hack for the same reason.
-    tl;dr - We want the non built scss files, and b/c this dep is only provided via
-    git, we use this hack. Please change it if you read this and know a better way.
-  */
-  pathBase(packageName) {
-    return path.dirname(
-      resolve.sync(`${packageName}/package.json`, { basedir: __dirname })
-    );
-  },
+  // pathBase(packageName) {
+  //   return path.dirname(
+  //     resolve.sync(`${packageName}/package.json`, { basedir: __dirname })
+  //   );
+  // },
 };

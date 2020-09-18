35bb5e0a4434941e72a2623d6e9fc5d55955508f
David Martinez de Silva <dmtzds@gmail.com>
2020-09-18 01:45:36 -0500

    ember eui image size prop

--- addon/components/eui-image/index.js
+++ addon/components/eui-image/index.js
@@ -1,13 +1,39 @@
 import GlimmerComponent from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { assert } from '@ember/debug';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
 
 export default class EmberEuiImage extends GlimmerComponent {
 
   url = '';
   alt = '';
-  size = '';
   @tracked hasShadow = this.args.hasShadow;
+
+  size = this.args.size;
+  @computed('size')
+  get sizeClass() {
+    if (typeof this.size == 'number') {
+      this.sizeStyle = htmlSafe(`max-height:${this.args.size}px; max-width:${this.args.size}px; width:auto;`);
+    } else {
+      switch (this.size) {
+        case 's':
+          return 'euiImage--small '
+        case 'm':
+          return 'euiImage--medium '
+        case 'l':
+          return 'euiImage--large '
+        case 'xl':
+          return 'euiImage--xlarge '
+        case 'fullWidth':
+          return 'euiImage--fullWidth '
+        case 'original':
+          break;
+        default:
+          return '';
+      }
+    }
+  }
 
   // caption = '';
   // fullScreenIconColor = '';

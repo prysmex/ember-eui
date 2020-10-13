26d1244124af368a0010a3d41e58e48d42061df3
betocantu93 <betocantu93@gmail.com>
2020-10-12 19:29:29 -0500

    rm accordion from PR

--- addon/utils/css-mappings/index.ts
+++ addon/utils/css-mappings/index.ts
@@ -1,11 +1,9 @@
 
-import EuiAccordion from './eui-accordion';
 import EuiIcon from './eui-icon';
 import EuiLoadingSpinner from './eui-loading-spinner';
 import EuiSpacer from './eui-spacer';
 
 const mapping: Mapping = {
-  EuiAccordion,
   EuiIcon,
   EuiLoadingSpinner,
   EuiSpacer

884907f076b011cc159ca9605a3c53a4c4fb77c3
betocantu93 <betocantu93@gmail.com>
2020-10-14 01:34:35 -0500

    allow strings as well as numbers

--- addon/components/eui-image/index.ts
+++ addon/components/eui-image/index.ts
@@ -9,26 +9,58 @@ type EuiImageSize = keyof typeof sizeMapping;
 
 type ImageSize = EuiImageSize | number;
 
+type FullScreenIconColor = 'light' | 'dark';
+
 type ImageArgs = {
+  /**
+   * Separate from the caption is a title on the alt tag itself.
+   * This one is required for accessibility.
+   */
+  alt: string;
+
   /*
     Determines the size of the image itself, can be a number or a string
     Default: 'm'
   */
-  size: ImageSize;
+  size?: ImageSize | number | string;
+
+  /**
+   * Changes the color of the icon that floats above the image when it can be clicked to fullscreen.
+   * The default value of `light` is fine unless your image has a white background, in which case you should change it to `dark`.
+   */
+  fullScreenIconColor?: FullScreenIconColor;
+
+  url: string;
+
+  /**
+   * Provides the visible caption to the image
+   */
+  caption?: string;
+  /**
+   * When set to `true` (default) will apply a slight shadow to the image
+   */
+  hasShadow?: boolean;
+  /**
+   * When set to `true` will make the image clickable to a larger version
+   */
+  allowFullScreen?: boolean;
+
 };
 
 export default class EmberEuiImage extends GlimmerComponent<ImageArgs> {
   @tracked isFullscreen = false;
-  @tracked sizeStyle: unknown = '';
   @argOrDefault('original') size!: ImageArgs['size'];
 
   get sizeClass(): string | undefined {
-    if (typeof this.size === 'number') {
-      this.sizeStyle = htmlSafe(
-        `max-height:${this.args.size}px; max-width:${this.args.size}px; width:auto;`
-      );
-    }
     return sizeMapping[this.size as EuiImageSize];
+  }
+
+  get sizeStyle() {
+    let size = this.args.size;
+    if ((typeof size === 'string' || typeof size === 'number') && !this.sizeClass) {
+      return htmlSafe(`max-height:${this.args.size}px; max-width:${this.args.size}px; width:auto;`);
+    }
+    return '';
   }
 
   @action

accd5572d794c44bc836ec8681a4e6324cfdfe6f
David Martinez de Silva <dmtzds@gmail.com>
2020-10-21 21:22:04 -0500

    selectable card stup

--- tests/dummy/app/controllers/demo/card.js
+++ tests/dummy/app/controllers/demo/card.js
@@ -1,11 +1,19 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class DemoIconController extends Controller {
+
+  @tracked cardOne = false;
 
 	@action
 	punchIt() {
 		console.log("You punched it into hyperspace!!");
+  }
+
+  @action
+  selectToggle(card){
+    card = !card;
   }
 
 }

f2f7b0d7162dbc53cc6389d1716a4e3b690ec5fe
David Martinez de Silva <dmtzds@gmail.com>
2020-10-23 19:00:26 -0500

    rename click modifier and made it more generic

---
+++ addon/modifiers/simulate-event.js
@@ -0,0 +1,19 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function simulateEvent(element, eventName, {handler, targetElement}) {
+
+  let _target = element.querySelector(`#${targetElement}`);
+
+  function _triggerEvent (target) {
+    let _newEvent = _target[handler];
+    typeof _newEvent === 'function' && _target[handler]();
+  }
+
+  if (targetElement) {
+    element.addEventListener(eventName, _triggerEvent);
+    return () => {
+      element.removeEventListener(eventName, _triggerEvent)
+    };
+  }
+
+});

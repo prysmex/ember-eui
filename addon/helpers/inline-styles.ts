51e15cf5aa2f02ea57abf6dad871738ce00278f3
David Martinez de Silva <dmtzds@gmail.com>
2020-10-28 16:45:02 -0600

    PR corrections

--- addon/helpers/inline-styles.ts
+++ addon/helpers/inline-styles.ts
@@ -2,15 +2,20 @@ import { helper } from '@ember/component/helper';
 import { assert } from '@ember/debug';
 import cssMappings from '../utils/css-mappings';
 
-export function inlineStyles(_:unknown, params:Record<string, unknown>) {
+interface InlineStylesParams {
+  componentName?: string;
+  [property: string]: unknown;
+}
+
+export function inlineStyles(_:unknown, params: InlineStylesParams) {
 
   let styles: string[] = [];
   const { componentName, ...properties } = params
 
   if (componentName) {
-    assert(`Could not find component ${componentName} in cssMappings.`, cssMappings[componentName as string]);
-    assert(`Could not find inlineStyles in ${componentName}'s cssMapping.`, cssMappings[componentName as string].inlineStyles);
-    styles.push(...cssMappings[componentName as string].inlineStyles?.(properties) as string[]);
+    assert(`Could not find component ${componentName} in cssMappings.`, cssMappings[componentName]);
+    assert(`Could not find inlineStyles in ${componentName}'s cssMapping.`, cssMappings[componentName].inlineStyles);
+    styles.push(...cssMappings[componentName].inlineStyles!?.(properties));
   }
 
   for(let property in properties) {

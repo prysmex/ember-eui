79bc6bc483eaeddc98cfc6843d584458bf138df6
betocantu93 <betocantu93@gmail.com>
2021-01-26 11:39:27 -0600

    simplify use-state

--- addon/helpers/use-state.ts
+++ addon/helpers/use-state.ts
@@ -1,18 +1,6 @@
 import { helper } from '@ember/component/helper';
-import { action, defineProperty } from '@ember/object';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { assert } from '@ember/debug';
-
-interface StatefulProperty {
-  get: () => unknown;
-  set: (value: unknown) => void;
-}
-
-function setterFn (context: MultipleUseState, prop: string): (value: unknown) => void {
-  return function (value) {
-    context[`${prop}Value`] = value;
-  }
-}
 
 /**
  * @param value - initial value to use, undefined if not provided.
@@ -21,127 +9,20 @@ function setterFn (context: MultipleUseState, prop: string): (value: unknown) =>
  *            `value` respectively.
  */
 class SingleUseState {
-  @tracked
-  value: unknown;
+  @tracked value: unknown;
 
-  constructor (value: unknown) {
+  constructor(value: unknown) {
     this.value = value;
-
-    defineProperty(this, 'get', {
-      get (this: SingleUseState) {
-        return this.value;
-      }
-    });
   }
 
   @action
-  set (value: unknown) {
+  setState(value: unknown) {
     this.value = value;
   }
 }
 
-/**
- * @param {Object} values - initial values to use, if a value is not provided
- *            undefined is used.
- *            The keys are the properties we want to be stateful.
- *
- * @returns - Object with each passed key in which each property has a `get`
- *            and `set` methods to retrieve and set the `value` respectively.
- */
-class MultipleUseState {
-  [property: string]: StatefulProperty | unknown;
-
-  constructor (values: Record<string, unknown>) {
-    Object.keys(values).forEach(prop => {
-      this.addProperty(prop, values[prop]);
-    });
-  }
-
-  hasProperty (prop: string): boolean {
-    return this.hasOwnProperty(prop);
-  }
-
-  addProperty (prop: string, initialValue?: unknown): void {
-    // Define the internal property for tracking
-    let baseDescriptor = {
-      writable: true,
-      enumerable: true,
-      configurable: true,
-      initializer: function () {
-        return initialValue;
-      }
-    };
-
-    let trackedDescriptor = tracked.call(null, this, `${prop}Value`, baseDescriptor) as unknown as PropertyDescriptor;
-    defineProperty(this, `${prop}Value`, trackedDescriptor);
-
-    // Create the object with `get` and `set` methods
-    const setFn = setterFn(this, prop);
-    let statefulProp = {
-      set: (value: unknown) => {
-        return setFn(value);
-      }
-    };
-
-    defineProperty(statefulProp, 'get', {
-      enumerable: true,
-      get: () => {
-        return this[`${prop}Value`];
-      }
-    });
-
-    this[prop] = statefulProp;
-  }
-}
-
-export function useState(
-  [...initialValues]: [unknown],
-  params: Record<string, unknown>
-  ): SingleUseState | MultipleUseState
-{
-  const paramsCount = Object.keys(params).length;
-  const hasParams = paramsCount > 0;
-  const hasMultipleValues = initialValues.length > 1;
-
-  // Must use the `MultipleUseState` class if there's more than 1 value
-  if (hasMultipleValues) {
-    // TODO: Allow multiple values? YOU DECIDE!!!
-    assert(`'use-state' can only be used to track one value. More than one value was provided: ${initialValues.join(', ')}`, !hasMultipleValues);
-
-    assert(`Incorrect use of 'use-state'.\n
-    Must be used with either an array of properties or hashed properties with
-    their initial values.\n
-    {{use-state var1 var2 ... varN}} or {{use-state var1=val1 var2=val2 ... varN=valN}}\n
-    When multiple strings are passed they are the names of the properties to set on the state;
-    these properties get initialized to 'undefined'.`, hasMultipleValues && !hasParams);
-
-    // We got an array, transpose it to an object
-    const values = initialValues.reduce<Record<string, unknown>>((acc, prop: string) => {
-      acc[prop] = undefined;
-      return acc;
-    }, {});
-    return new MultipleUseState(values);
-  } else if (hasParams) {
-    // TODO: Allow multiple values? YOU DECIDE!!!
-    assert(`'use-state' can only be used to track one value. More than one value was provided: ${Object.keys(params).join(', ')}`, paramsCount === 1);
-
-    assert(`Incorrect use of 'use-state'.\n
-    Must be used with either an array of properties or hashed properties with
-    their initial values.\n
-    {{use-state var1 var2 ... varN}} or {{use-state var1=val1 var2=val2 ... varN=valN}}
-    `, !hasMultipleValues && hasParams);
-
-    if (paramsCount === 1) {
-      // This can be replaced for `Object.values` however that would need a
-      // polyfill on some browsers
-      const value = params[Object.keys(params)[0]];
-      return new SingleUseState(value);
-    }
-
-    return new MultipleUseState(params);
-  }
-
-  return new SingleUseState(initialValues[0]);
+export function useState([initialValue]: [unknown]): SingleUseState {
+  return new SingleUseState(initialValue);
 }
 
 export default helper(useState);

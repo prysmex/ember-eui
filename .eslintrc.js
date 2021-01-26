ee9c67b302d093535db50db5708af70922e79283
betocantu93 <betocantu93@gmail.com>
2021-01-26 13:23:45 -0600

    prettier and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,13 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ['ember'],
+  extends: ['eslint:recommended', 'ember/recommended', 'prettier'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {},
   overrides: [
@@ -32,23 +27,18 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'tests/dummy/config/**/*.js'
+        'tests/dummy/config/**/*.js',
       ],
-      excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
-      ],
+      excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
       },
       env: {
         browser: false,
-        node: true
+        node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended']
-    }
-  ]
+      plugins: ['node', 'ember', 'prettier'],
+      extends: ['plugin:node/recommended'],
+    },
+  ],
 };

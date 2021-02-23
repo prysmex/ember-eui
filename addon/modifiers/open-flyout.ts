5d8402f3e121837f13324f0e6b5100cd6a1679bf
betocantu93 <betocantu93@gmail.com>
2021-02-23 12:53:33 -0600

    use set-body-class helper

--- addon/modifiers/open-flyout.ts
+++
@@ -1,9 +0,0 @@
-import { modifier } from 'ember-modifier';
-
-export default modifier(function openFlyout(_) {
-  document.body.classList.add('euiBody--hasFlyout');
-
-  return () => {
-    document.body.classList.remove('euiBody--hasFlyout');
-  };
-});

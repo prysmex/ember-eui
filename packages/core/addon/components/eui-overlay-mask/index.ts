f044497de43d8c87b06e7dae24e1eb8ede388a9a
betocantu93 <betocantu93@gmail.com>
2021-06-04 16:29:07 -0500

    fix: :bug: stacking modals remove class

--- packages/core/addon/components/eui-overlay-mask/index.ts
+++ packages/core/addon/components/eui-overlay-mask/index.ts
@@ -1,18 +1,6 @@
 import GlimmerComponent from '@glimmer/component';
 
 interface EuiOverlayMaskArgs {}
-
 export default class EuiOverlayMaskComponent extends GlimmerComponent<EuiOverlayMaskArgs> {
   destinationElement = document.body;
-
-  constructor(owner: unknown, args: EuiOverlayMaskArgs) {
-    super(owner, args);
-    const body = document.body;
-    body.classList.add('euiBody-hasOverlayMask');
-  }
-
-  willDestroy(): void {
-    const body = document.body;
-    body.classList.remove('euiBody-hasOverlayMask');
-  }
 }

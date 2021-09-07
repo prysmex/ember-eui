56cf11138a2b116cd6ec39f687dfb46d6307975e
betocantu93 <betocantu93@gmail.com>
2021-09-07 17:54:09 -0500

    fix: do not avoid arrays

--- packages/validated-form/addon/components/validated-form/field-combo-box/index.js
+++ packages/validated-form/addon/components/validated-form/field-combo-box/index.js
@@ -1,17 +1,12 @@
 import ValidatedFormFieldBase from '../field-base';
 import { action } from '@ember/object';
-import { isArray } from '@ember/array';
 
 export default class ValidatedFormFieldComboBox extends ValidatedFormFieldBase {
   validationProperty = 'args.selectedOptions';
 
   @action
   handleChange(options) {
-    let value = options;
-    if (this.args.singleSelection) {
-      value = isArray(options) ? options[0] : null;
-    }
-    this.args.onChange(value);
+    this.args.onChange(options);
     this.notifyValidityChange();
   }
 }

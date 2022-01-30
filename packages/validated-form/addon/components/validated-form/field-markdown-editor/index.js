7b66250c821a51eb642da8af683df6338b04755a
betocantu93 <betocantu93@gmail.com>
2022-01-29 22:58:23 -0600

    simplify

--- packages/validated-form/addon/components/validated-form/field-markdown-editor/index.js
+++ packages/validated-form/addon/components/validated-form/field-markdown-editor/index.js
@@ -4,6 +4,7 @@ import ValidatedFormFieldBase from '../field-base';
 export default class ValidatedFormFieldMarkdownEditor extends ValidatedFormFieldBase {
   @action
   handleChange(str) {
-    super.handleChange({ target: { value: str } });
+    this.args.onChange(str);
+    this.notifyValidityChange();
   }
 }

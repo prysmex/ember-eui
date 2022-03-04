bb961d28ba5579a7fbad77a1032a2b9829e1b8b0
betocantu93 <betocantu93@gmail.com>
2022-03-04 11:36:51 -0600

    fix: make eui-pikaday more flexible

--- packages/pikaday/addon/components/eui-pikaday/index.js
+++ packages/pikaday/addon/components/eui-pikaday/index.js
@@ -2,6 +2,8 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class EuiPikadayComponent extends Component {
+  field = null;
+
   get format() {
     return this.args.format || 'DD.MM.YYYY';
   }
@@ -48,6 +50,11 @@ export default class EuiPikadayComponent extends Component {
   }
 
   @action
+  registerField(field) {
+    this.field = field;
+  }
+
+  @action
   onClose() {
     if (this.isDestroying) {
       return;
@@ -56,6 +63,14 @@ export default class EuiPikadayComponent extends Component {
       this.onSelect(null);
     }
     this.args.onClose?.();
+  }
+
+  @action
+  onOpen() {
+    if (this.isDestroying) {
+      return;
+    }
+    this.args.onOpen?.();
   }
 
   #heardValue;

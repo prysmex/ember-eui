86e8ebebbd8b43ab282999757e2e21c9df4d25a3
betocantu93 <betocantu93@gmail.com>
2023-11-03 12:33:45 +0000

    mv

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -15,22 +15,23 @@ export default {
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
     addon.publicEntrypoints([
+      'components/**/*.js',
       'helpers/**/*.js',
+      'utils/**/*.js',
       'modifiers/**/*.js',
       'services/**/*.js',
-      'components/**/*.js',
-      'utils/**/*.js'
+      'template-registry.js'
     ]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
     addon.appReexports([
-      'helpers/**/*.js',
-      'modifiers/**/*.js',
-      'services/**/*.js',
       'components/**/*.js',
-      'utils/**/*.js'
+      'helpers/**/*.js',
+      'utils/**/*.js',
+      'modifiers/**/*.js',
+      'services/**/*.js'
     ]),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
